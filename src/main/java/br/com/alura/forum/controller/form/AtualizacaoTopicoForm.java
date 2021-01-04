2438624cc6a433c9fb2de1d59194c5d4d772ba62
Bernardo Camargo <bernardopcamargo@gmail.com>
2021-01-04 08:57:13 -0300

    first commit

---
+++ src/main/java/br/com/alura/forum/controller/form/AtualizacaoTopicoForm.java
@@ -0,0 +1,43 @@
+package br.com.alura.forum.controller.form;
+
+import javax.validation.constraints.NotNull;
+
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+
+public class AtualizacaoTopicoForm {
+
+	@NotNull
+	private String titulo;
+	@NotNull
+	private String mensagem;
+	@NotNull	
+	private String nomeCurso;
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getMensagem() {
+		return mensagem;
+	}
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	public String getNomeCurso() {
+		return nomeCurso;
+	}
+	public void setNomeCurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
+	}
+	
+	public Topico atualizar(Long id, TopicoRepository topicoRepository) {
+		Topico topico = topicoRepository.getOne(id);
+		topico.setTitulo(this.titulo);
+		topico.setMensagem(this.mensagem);
+		return topico;
+	}
+	
+	
+}
